4165b09cad1f1414a9b97e26207dd312b8437ebc
Monique Funch <mon@Moniques-MacBook-Pro.local>
2021-02-25 13:44:37 +0100

    javascript med kommentarer

--- mainscript.js
+++ mainscript.js
@@ -1,105 +1,112 @@
- const header = document.querySelector("header h1");
- //        const url = "https://kbhsteder-1f70.restdb.io/rest/steder"
- const medieurl = "https://kbhsteder-1f70.restdb.io/media/";
- const myHeaders = {
-     "x-apikey": "602f89cb5ad3610fb5bb63c7"
- }
+//medieurl og x-apikey indsættes og defineres
+const header = document.querySelector("header h1");
+const medieurl = "https://kbhsteder-1f70.restdb.io/media/";
+const myHeaders = {
+    "x-apikey": "602f89cb5ad3610fb5bb63c7"
+}
 
- document.addEventListener("DOMContentLoaded", start)
- let steder;
- let filter = "alle";
- let counter = 0;
-
-
- /*-----------SLIDESHOW----------*/
-
- var myIndex = 0;
+//DOM content skal loades og variabler bliver defineret
+document.addEventListener("DOMContentLoaded", start)
+let steder;
+let filter = "alle";
+let counter = 0;
 
 
- function carousel() {
-     var i;
-     var x = document.getElementsByClassName("mySlides");
-     for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";
-     }
-     myIndex++;
-     if (myIndex > x.length) {
-         myIndex = 1
-     }
-     x[myIndex - 1].style.display = "block";
-     setTimeout(carousel, 5000); // Change image every 2 seconds
- }
+/*-----------SLIDESHOW----------*/
+var myIndex = 0;
 
- /*-------------------BURGER-MENU---------------*/
+//kode lånt fra W3 schools, for i betyder forEach
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
 
- window.addEventListener("load", sidenVises);
- const navButtons = document.querySelectorAll(".nav-btns li");
+/*-------------------BURGER-MENU---------------*/
 
- function sidenVises() {
-     console.log("sidenVises");
-     document.querySelector(".burger").addEventListener("click", toggleMenu);
- }
+window.addEventListener("load", sidenVises);
 
- function toggleMenu() {
-     console.log("toggleMenu");
-     const burger = document.querySelector('.burger');
+// funktionen lytter efter klik på burgermenuen
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector(".burger").addEventListener("click", toggleMenu);
+}
 
-     document.querySelector(".nav-btns").classList.toggle("nav-active");
+// funktionen toggler burgermenuen så den slåes til og fra
+function toggleMenu() {
+    console.log("toggleMenu");
+    const burger = document.querySelector('.burger');
 
-     //Burger kryds
-     burger.classList.toggle('toggle');
- }
+    document.querySelector(".nav-btns").classList.toggle("nav-active");
 
- /*-------------------FILTRERING FUNKTIONER---------------*/
+    //mulighed for at toggle på burger kryds, får tildelt klassen toggle
+    burger.classList.toggle('toggle');
+}
 
- function start() {
-     console.log("DOM er loadet");
-     const filterKnapper = document.querySelectorAll("nav button");
-     filterKnapper.forEach(knap => knap.addEventListener("click", filtrerSteder));
-     loadJSON();
-     carousel();
- }
-
- function filtrerSteder() {
-     filter = this.dataset.kategori;
-     console.log("filter", filter);
-     document.querySelector(".valgt").classList.remove("valgt");
-     this.classList.add("valgt")
-     visSteder();
-     header.textContent = this.textContent;
- }
+/*-------------------FILTRERING FUNKTIONER---------------*/
 
 
- async function loadJSON() {
-     const JSONData = await fetch("https://kbhsteder-1f70.restdb.io/rest/steder", {
-         headers: myHeaders
-     });
-     steder = await JSONData.json();
-     console.log("steder", steder);
-     visSteder();
- }
+//DOM er loadet, vi hiver fat i alle filtrerknapper og giver dem hver især er  eventlistener så de kan klikkes på
+function start() {
+    console.log("DOM er loadet");
+    const filterKnapper = document.querySelectorAll("nav button");
+    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerSteder));
+    loadJSON();
+    carousel();
+}
 
- function visSteder() {
-     const dest = document.querySelector("#liste");
-     const skabelon = document.querySelector("template").content;
-     dest.textContent = "";
-     steder.forEach(sted => {
-         if (filter == sted.bydel || filter == "alle") {
-             const klon = skabelon.cloneNode(true);
-             klon.querySelector(".billede").src = medieurl + sted.billede[0];
-             klon.querySelector(".navn").textContent = sted.navn;
-             klon.querySelector(".kortbeskrivelse").textContent = sted.kortbeskrivelse;
-             klon.querySelector(".adresse").textContent = `${"Adresse: "}` + sted.adresse;
-             klon.querySelector(".kategori").textContent = sted.kategori;
+// filtrering af steder defineres og fjerner valgt classen og tilføjer igen
+function filtrerSteder() {
+    filter = this.dataset.kategori;
+    console.log("filter", filter);
+    document.querySelector(".valgt").classList.remove("valgt");
+    this.classList.add("valgt")
+    visSteder();
+    header.textContent = this.textContent;
+}
 
-             klon.querySelector(".steder").addEventListener("click", () => visDetaljer(sted));
+//asynkron funktion hvor Json fetches og data hentes ned
+async function loadJSON() {
+    const JSONData = await fetch("https://kbhsteder-1f70.restdb.io/rest/steder", {
+        headers: myHeaders
+    });
+    steder = await JSONData.json();
+    console.log("steder", steder);
+    visSteder();
+}
 
-             dest.appendChild(klon);
-         }
-     })
- }
+// tekst og billeder bliver sat ind fra restDB
+function visSteder() {
+    const dest = document.querySelector("#liste");
+    const skabelon = document.querySelector("template").content;
+    dest.textContent = ""; //HTML containeren tømmes så der kan fyldes nyt indhold i den
+    steder.forEach(sted => {
+        //her loopes der igennem arrayet
+        if (filter == sted.bydel || filter == "alle") {
+            const klon = skabelon.cloneNode(true); //templaten bliver klonet og der kommer nyt indhold i den fra restdb
+            klon.querySelector(".billede").src = medieurl + sted.billede[0];
+            klon.querySelector(".navn").textContent = sted.navn;
+            klon.querySelector(".kortbeskrivelse").textContent = sted.kortbeskrivelse;
+            klon.querySelector(".adresse").textContent = `${"Adresse: "}` + sted.adresse;
+            klon.querySelector(".kategori").textContent = sted.kategori;
 
- function visDetaljer(hvad) {
-     location.href = `singleview.html?id=${hvad._id}`;
+            klon.querySelector(".steder").addEventListener("click", () => visDetaljer(sted));
 
- }
+            dest.appendChild(klon); //Klonen tilføjes til DOM'en
+        }
+    })
+}
+
+//der linkes til singleview med det specifikke ID
+function visDetaljer(hvad) {
+    location.href = `singleview.html?id=${hvad._id}`;
+
+}
